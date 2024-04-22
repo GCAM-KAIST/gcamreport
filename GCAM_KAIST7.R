27d7860271159d5efb86913fe46023535ffaf503
kaist-iam <coreanjs@naver.com>
2024-04-22 10:37:35 +0900

    R file name change

--- GCAM_KAIST7.R
+++ GCAM_KAIST7.R
@@ -7,7 +7,7 @@ devtools::load_all()
 generate_report(db_path ="E:/gcam-v7.0-Windows-Release-Package_GGS621/",  # path to database under gcamreport folder
                 db_name = "testdb",   # db folder name
                 scenarios = c('Reference', 'NZ_Electricity_Nuc_Policy'), ## scenario names
-                prj_name = "GCAM-KAIST2.0.dat", final_year = 2100,
+                prj_name = "GCAM-KAIST7.dat", final_year = 2100,
                 desired_regions = c('South Korea', 'Japan'),
                 desired_variables = c(
                   #'Agricultural*',
